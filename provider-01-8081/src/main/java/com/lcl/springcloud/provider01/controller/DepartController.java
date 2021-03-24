6521c540d84d04ca74f346961f43cb7434d294a7
liconglong <liconglong@jd.com>
2021-03-24 17:03:07 +0800

    springcloud

--- provider-01-8081/src/main/java/com/lcl/springcloud/provider01/controller/DepartController.java
+++ provider-01-8081/src/main/java/com/lcl/springcloud/provider01/controller/DepartController.java
@@ -2,14 +2,18 @@ package com.lcl.springcloud.provider01.controller;
 
 import com.lcl.springcloud.provider01.dao.Depart;
 import com.lcl.springcloud.provider01.service.DepartService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/depart")
+@Slf4j
 public class DepartController {
 
     @Autowired
@@ -41,6 +45,25 @@ public class DepartController {
     @GetMapping("/findAll")
     public List<Depart> findAll(){
         return departService.findAll();
+    }
+
+    @GetMapping("/services")
+    public void getServices(){
+        List list = new ArrayList();
+        List<String> services = client.getServices();
+        for (String serviceNanme: services) {
+            List<ServiceInstance> instances = client.getInstances(serviceNanme);
+            for (ServiceInstance instance : instances) {
+                log.info("instance.getHost()==================={}", instance.getHost());
+                log.info("instance.getInstanceId()==================={}", instance.getInstanceId());
+                log.info("instance.getServiceId()==================={}", instance.getServiceId());
+                log.info("instance.getUri()==================={}", instance.getUri());
+                log.info("instance.getMetadata()==================={}", instance.getMetadata());
+                log.info("instance.getPort()==================={}", instance.getPort());
+                log.info("instance.getScheme()==================={}", instance.getScheme());
+            }
+        }
+
     }
 
 }
