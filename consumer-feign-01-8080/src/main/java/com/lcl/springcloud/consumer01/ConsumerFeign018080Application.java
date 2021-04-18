b0a031ad43d9a12b9e36c2ebea8f286db0cdb6be
liconglong@163.com <liconglong@163.com>
2021-04-18 21:16:09 +0800

    Hystrix

--- consumer-feign-01-8080/src/main/java/com/lcl/springcloud/consumer01/ConsumerFeign018080Application.java
+++ consumer-feign-01-8080/src/main/java/com/lcl/springcloud/consumer01/ConsumerFeign018080Application.java
@@ -1,14 +1,14 @@
 package com.lcl.springcloud.consumer01;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableEurekaClient
+//@SpringBootApplication
+//@EnableCircuitBreaker
+//@EnableEurekaClient
 @EnableFeignClients("com.lcl.springcloud.consumer01.service")
+@SpringCloudApplication
 public class ConsumerFeign018080Application {
 
     public static void main(String[] args) {
